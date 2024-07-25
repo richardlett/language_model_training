9a5efa87f4647aa55935f263af171ac048a7e1aa
richardlett <59459734+richardlett@users.noreply.github.com>
2024-07-24 18:28:24 -0700

    Update jobscript_cross_valid_example.py

--- jobscript_cross_valid_example.py
+++ jobscript_cross_valid_example.py
@@ -4,9 +4,4 @@ echo "rank" $SLURM_PROCID
 export ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 echo "Addr" $ADDR
 
-#source ~/.bashrc_backup
-#mamba activate /pscratch/sd/r/richardl/gnn_binning_cad2
-#module load nccl
-#export ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-
-torchrun --nnodes=$SLURM_JOB_NUM_NODES --nproc_per_node=4 --node_rank=$SLURM_PROCID --rdzv_id=32 --rdzv_endpoint=$ADDR:12351 cross_valid_example.py
+torchrun --nnodes=$SLURM_JOB_NUM_NODES --nproc_per_node=4 --node_rank=$SLURM_PROCID --rdzv_id=32 --rdzv_endpoint=$ADDR:12351 train_model_cross_valid_example.py
